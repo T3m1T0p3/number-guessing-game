38eb94f280c1915971d253d1df293c7d86713d4e
T3m1T0p3 <temmythoper@gmail.com>
2022-05-10 18:14:36 +0000

    feat: welcome back

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,54 @@ PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
 echo "Enter your username:"
 read username
 number=$(( $RANDOM%1001 ))
+
+function check(){
+  if [[ $1 =~ [^0-9] ]]
+  then
+   printf "That is not an integer, guess again:\n"
+   return 1
+  fi
+  if [[ $1 > $number ]]
+  then
+   printf "It's lower than that, guess again:\n"
+   return 1
+  elif [[ $1 < $number ]]
+  then
+    printf "It's higher than that, guess again:\n"
+    return 1
+  else
+   print "You guessed it in "$2" tries. The secret number was "$1". Nice job!\n"
+   return 0;
+  fi
+}
+
+
+if [[ -z $username ]]
+then
+ echo "Welcome, "$username"! It looks like this is your first time here."
+ echo "Guess the secret number between 1 and 1000:"
+ read userguess
+ check $userguess $guess
+ call=$?
+ while [[ call != 0 ]]
+ do
+  read userguess
+  guess=$(( $guess + 1 ))
+  check $userguess $guess
+  call=$?
+ done
+else
+ games_played=$($PSQL "SELECT game_played FROM games WHERE username="$query"")
+ best_game=$($PSQL "SELECT MIN(best_game) FROM games WHERE username="$query"")
+ echo "Welcome back, "$query"! You have played  "$games_played", and your best game took <best_game> guesses."
+ read userguess
+ check $userguess
+ call=$?
+ while [[ call != 0 ]]
+ do
+  read userguess
+  guess=$(( $guess + 1 ))
+  check $userguess $guess
+  call=$?
+ done
+fi
